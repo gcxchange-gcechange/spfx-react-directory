ba8c8e637d16c04fcd23fa5b646bd643112c4a67
MoonSky1! <vanmathyrr@gmail.com>
2022-12-29 10:14:09 -0500

    Initial Commit

---
+++ src/webparts/spfxReactDirectoryWebpart/components/ISpfxReactDirectoryWebpartProps.ts
@@ -0,0 +1,7 @@
+export interface ISpfxReactDirectoryWebpartProps {
+  description: string;
+  isDarkTheme: boolean;
+  environmentMessage: string;
+  hasTeamsContext: boolean;
+  userDisplayName: string;
+}
