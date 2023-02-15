39d2d6073da2864779ff54d67bfde4cbe918d880
RVany <vanmathyrr@gmail.com>
2023-02-15 10:32:16 -0500

    Serarch box label and place holder text are changed as requested.

--- src/webparts/spfxReactDirectoryWebpart/loc/en-us.js
+++ src/webparts/spfxReactDirectoryWebpart/loc/en-us.js
@@ -1,6 +1,6 @@
 define([], function () {
   return {
-    SearchPlaceHolder: "Search by first name",
+    SearchPlaceHolder: "Search by last name",
     PropertyPaneDescription: "Search People from Organization Directory",
     BasicGroupName: "Properties",
     TitleFieldLabel: "Web Part Title",
@@ -8,7 +8,7 @@ define([], function () {
     DirectoryMessage:
       "<h3>We couldn't find the user you were looking for.</h3> They might not yet have joined GCXchange. Why not invite them, so you can work better, together.",
     LoadingText: "Searching for user. Please wait...",
-    SearchBoxLabel: "Search for a colleague (First Name)",
+    SearchBoxLabel: "Search for a colleague (Last Name)",
     SearchButtonLabel: "Search",
     SendEmailLabel: "Send Email",
     NoUserFoundLabelText: "Invite a colleague",
