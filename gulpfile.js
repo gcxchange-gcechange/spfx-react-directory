49acd1f361d7f9a53005b5288e67281124df817d
MoonSky1! <vanmathyrr@gmail.com>
2023-11-17 14:53:25 -0500

    More cli report steps completed

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
-'use strict';
+"use strict";
 
-const build = require('@microsoft/sp-build-web');
+const build = require("@microsoft/sp-build-web");
 
 build.addSuppression(`Warning - [sass] The local CSS class 'ms-Grid' is not camelCase and will not be type-safe.`);
 
@@ -8,9 +8,9 @@ var getTasks = build.rig.getTasks;
 build.rig.getTasks = function () {
   var result = getTasks.call(build.rig);
 
-  result.set('serve', result.get('serve-deprecated'));
+  result.set("serve", result.get("serve-deprecated"));
 
   return result;
 };
 
-build.initialize(require('gulp'));
+build.initialize(require("gulp"));
