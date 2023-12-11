3fa9f7fb3898e73b934b369af209d1e1fd7159b8
RVany <vanmathyrr@gmail.com>
2023-12-11 16:26:02 -0500

    Chat link text modified as requested

--- src/webparts/spfxReactDirectoryWebpart/components/PersonaCard/PersonaCard.tsx
+++ src/webparts/spfxReactDirectoryWebpart/components/PersonaCard/PersonaCard.tsx
@@ -116,7 +116,7 @@ export class PersonaCard extends React.Component<IPersonaCardProps, IPersonaCard
                       target="_blank"
                       rel="noreferrer"
                     >
-                      MS Teams Chat
+                      Chat on MS Teams
                     </a>
                   }
                 </span>
@@ -133,7 +133,7 @@ export class PersonaCard extends React.Component<IPersonaCardProps, IPersonaCard
                         CreateChat(this.props.profileProperties.Id);
                       }}
                     >
-                      Create MS Teams Chat
+                      Chat on MS Teams
                     </a>
                   }
                 </span>
